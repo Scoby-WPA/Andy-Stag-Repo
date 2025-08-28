6fec92848561375a046ea0aaa86005572a3c2603
Scoby-WPA <mickschofield@gmail.com>
2025-08-28 14:54:38 +0100

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,4 @@
+
 importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js');
 firebase.initializeApp({
