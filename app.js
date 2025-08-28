adb05fe8f28e97dfd968ec3b075b501f74844db6
Scoby-WPA <mickschofield@gmail.com>
2025-08-28 14:12:18 +0100

    Add full app files for Andy’s Stag Bingo web app

---
+++ app.js
@@ -0,0 +1,11 @@
+// Defaults: Firebase + Game Code
+const DEFAULT_FB_CONFIG = {
+  apiKey: "AIzaSyCaUvTFms4R8ufS4jxXw0M0AtAQ763RlMc",
+  authDomain: "andystag25.firebaseapp.com",
+  projectId: "andystag25",
+  storageBucket: "andystag25.firebasestorage.app",
+  messagingSenderId: "43167349544",
+  appId: "1:43167349544:web:f3ec01160a0166fc2a6e1d",
+  measurementId: "G-LJ54DX0R9Q"
+};
+const DEFAULT_GAME_CODE = 'andystag25';
---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e=>{e.waitUntil(caches.open('stag-bingo-v16').then(c=>c.addAll(['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/icon-192.png','/icon-512.png','/firebase-messaging-sw.js'])))})
+self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
